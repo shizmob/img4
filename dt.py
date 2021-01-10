3589c32027f9d12f9e3f1649a4d42ee5ae269208
Shiz <hi@shiz.me>
2021-01-10 01:38:16 +0100

    dt: parse some meaningful values

--- dt.py
+++ dt.py
@@ -12,8 +12,8 @@ class Property(restruct.Struct, partials={'D'}):
 class Node(restruct.Struct, partials={'P', 'C'}):
     property_count: UInt(32) @ P.count
     child_count:    UInt(32) @ C.count
-    properties:      Arr(AlignTo(Property, 4)) @ P
-    children:        Arr(...) @ C
+    properties:     Arr(AlignTo(Property, 4)) @ P
+    children:       Arr(...) @ C
 
 # recursionnn
 restruct.to_type(Node).fields['children'].type = Node
@@ -22,10 +22,21 @@ restruct.to_type(Node).fields['children'].type = Node
 if __name__ == '__main__':
     import sys
 
+    formats = {
+        'compatible': restruct.Arr(restruct.Str(), stop_value=''),
+        'platform-name': restruct.Str(),
+        'name': restruct.Str(),
+        '#size-cells': restruct.UInt(32),
+        '#address-cells': restruct.UInt(32),
+        'AAPL,phandle': restruct.UInt(32),
+    }
+
     def isprint(x):
         return all(0x7E >= b >= 0x20 for b in x)
 
-    def print_nice(v):
+    def to_nice(k, v):
+        if k in formats:
+            return restruct.format_value(restruct.parse(formats[k], v), str)
         stripped = v.rstrip(b'\x00')
         if stripped and isprint(stripped):
             return stripped.decode('ascii')
@@ -35,9 +46,9 @@ if __name__ == '__main__':
         space = ' ' * (depth * 2)
         props = {x.name: x.value for x in n.properties}
         name = props.pop('name')
-        print(space + '+-', print_nice(name))
+        print(space + '+-', to_nice('name', name))
         for k, v in props.items():
-            print(space + '|   ', k + ':', print_nice(v))
+            print(space + '|   ', k + ':', to_nice(k, v))
         if n.children:
             print(space + '\\_,')
             for c in n.children:
