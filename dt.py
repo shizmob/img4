ea3b54841231e971d39815ccbf0de19466d260d6
Shiz <hi@shiz.me>
2021-01-10 01:26:33 +0100

    dt: enable nicer printing

--- dt.py
+++ dt.py
@@ -21,4 +21,26 @@ restruct.to_type(Node).fields['children'].type = Node
 
 if __name__ == '__main__':
     import sys
-    print(restruct.parse(Node, open(sys.argv[1], 'rb')))
+
+    def isprint(x):
+        return all(0x7E >= b >= 0x20 for b in x)
+
+    def print_nice(v):
+        stripped = v.rstrip(b'\x00')
+        if stripped and isprint(stripped):
+            return stripped.decode('ascii')
+        return restruct.format_bytes(v)
+
+    def print_node(n, depth=0):
+        space = ' ' * (depth * 2)
+        props = {x.name: x.value for x in n.properties}
+        name = props.pop('name')
+        print(space + '+-', print_nice(name))
+        for k, v in props.items():
+            print(space + '|   ', k + ':', print_nice(v))
+        if n.children:
+            print(space + '\\_,')
+            for c in n.children:
+                print_node(c, depth=depth + 1)
+    dt = restruct.parse(Node, open(sys.argv[1], 'rb'))
+    print_node(dt)
